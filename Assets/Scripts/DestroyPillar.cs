3e2e31db85c0ee05ffe9d23bda8eaf4333d4839f
Nate Smith <me@nwlsmith.com>
2020-12-06 20:16:57 -0500

    Added destructible pillars

---
+++ Assets/Scripts/DestroyPillar.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPillar : MonoBehaviour
+{
+
+    [SerializeField] private GameObject pillarModel = null;
+    [SerializeField] private GameObject pillarModelBroken1 = null;
+    [SerializeField] private GameObject pillarModelBroken2 = null;
+    [SerializeField] private GameObject glowingDynamite = null;
+    [SerializeField] private GameObject notGlowingDynamite = null;
+    [SerializeField] private GameObject explosion = null; 
+
+    [SerializeField] private float countdownTime = 5f;
+    private AudioSource audioSource;
+
+    private bool exploding = false;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void TriggerExplosionCountdown()
+    {
+        if (!exploding)
+            StartCoroutine(ExplosionCountdownEnum());
+    }
+
+    private IEnumerator ExplosionCountdownEnum()
+    {
+        glowingDynamite.SetActive(false);
+        notGlowingDynamite.SetActive(true);
+
+        yield return new WaitForSeconds(countdownTime);
+
+        notGlowingDynamite.SetActive(false);
+        explosion.SetActive(true);
+        explosion.GetComponent<ParticleSystem>().Play();
+        audioSource.Play();
+
+        pillarModel.SetActive(false);
+        pillarModelBroken1.SetActive(true);
+        pillarModelBroken2.SetActive(true);
+
+
+
+        yield return new WaitForSeconds(.25f);
+
+        explosion.GetComponent<Light>().enabled = false;
+
+        GameManager.instance.ExplodedPillar();
+    }
+}
