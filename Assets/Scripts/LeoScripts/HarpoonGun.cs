b56d865803aa47895014ffc7b938c026afc0081d
FINNJC <finnjcarney@gmail.com>
2020-12-10 17:16:25 -0500

    Fixing Things Commit! I hope

---
+++ Assets/Scripts/LeoScripts/HarpoonGun.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarpoonGun : MonoBehaviour
+{
+    public static HarpoonGun instance;
+    public GameObject LockedTarget;
+    public Rigidbody RB;
+
+    public GameObject HarpoonPrefab;
+    public GameObject Harpoon;
+    public GameObject RopePrefab;
+    public GameObject Rope;
+
+    void Awake() 
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (Harpoon == null)
+            {
+                Instantiate(HarpoonPrefab, transform.position, transform.rotation);
+            }
+            if (Rope == null) 
+            {
+                Instantiate(RopePrefab, transform.position, transform.rotation);
+            }
+        }
+        if (Input.GetMouseButtonUp(0)) 
+        {
+            Destroy(Harpoon);
+            Destroy(Rope);
+        }
+
+    }
+}
