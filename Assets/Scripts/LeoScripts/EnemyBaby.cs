b50789f59601c9ff942c73ded5d5cd1a0fff1d98
Primary <yw3726@nyu.com>
2020-12-12 15:22:10 -0500

    Harpoon Updates, baby attacks.

---
+++ Assets/Scripts/LeoScripts/EnemyBaby.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBaby : MonoBehaviour
+{
+    public bool Attacking; //If the baby is charging
+    public Rigidbody MyRB;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Attacking) 
+        {
+            transform.LookAt(PlayerManager.instance.transform);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (Attacking && collision.gameObject.tag == "Player") 
+        {
+            Debug.Log("Attacked Player");
+            MyRB.AddForce(transform.forward * - 300);
+            //Call Player take damage method
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Attacking)
+        {
+            MyRB.AddForce(transform.forward * 1);
+        }
+    }
+
+}
