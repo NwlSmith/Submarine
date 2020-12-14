f0d9d843941bad38313e349b7bcff05b28ca61bf
Nate Smith <me@nwlsmith.com>
2020-12-13 20:14:14 -0500

    Added tons of sounds: Motor Water ambiance bomb explosion bomb place bomb fuse boss seeing player rumble
    Added code for:
    Bombs pushing the player away if theyre too close
    text for planting bombs
    player sound manager
    player light manager
    more shaking code in CameraFX
    Fleshed out the boss more, it now has sounds, camera effects, and grabs and pushes the player
    fixed up the pillar
    added stuff to gamemanager

--- Assets/Scripts/BossSight.cs
+++ Assets/Scripts/BossSight.cs
@@ -10,43 +10,55 @@ using UnityEngine;
 
 public class BossSight : MonoBehaviour
 {
+    public bool activated = true;
     public bool seesPlayer = false;
     private float timeSeeingPlayer = 0;
+    private GameObject lightHolder;
     [SerializeField] private PlayerManager playerManager;
 
     // Start is called before the first frame update
     void Start()
     {
         playerManager = PlayerManager.instance;
+        lightHolder = GetComponentInChildren<Light>().gameObject;
+        lightHolder.GetComponent<Light>().intensity = 0;
     }
 
     private void FixedUpdate()
     {
-        Ray bossToPlayer = new Ray(playerManager.transform.position, (transform.position - playerManager.transform.position).normalized);
-        if(Physics.Raycast(bossToPlayer, out RaycastHit hit, 50f))
+        if (activated)
         {
-            // if the boss has uninterrupted line of sight to player
-            if (hit.collider.CompareTag("Player"))
+            Ray bossToPlayer = new Ray(transform.position, (playerManager.transform.position - transform.position).normalized);
+            if (Physics.Raycast(bossToPlayer, out RaycastHit hit, 50f))
             {
-                timeSeeingPlayer += Time.fixedDeltaTime;
-                Debug.Log("timeSeeingPlayer : " + timeSeeingPlayer);
-                // if the boss couldn't previously see the player
-                if (!seesPlayer)
+                // if the boss has uninterrupted line of sight to player
+                if (hit.collider.CompareTag("Player"))
                 {
-                    // trigger boss seeing player
-                    CaughtPlayer();
-                }
+                    timeSeeingPlayer += Time.fixedDeltaTime;
 
-                if (timeSeeingPlayer > GameManager.instance.numPillars)
+                    lightHolder.transform.LookAt(playerManager.transform);
+
+                    // if the boss couldn't previously see the player
+                    if (!seesPlayer)
+                    {
+                        // trigger boss seeing player
+                        CaughtPlayer();
+                    }
+
+                    if (timeSeeingPlayer > GameManager.instance.numPillars)
+                    {
+                        BossSeenTooLong();
+                    }
+                }
+                else if (seesPlayer)
                 {
-                    Debug.Log("Had sight of player for too long");
-                    playerManager.BossSeenTooLong();
+                    LostPlayer();
                 }
             }
-            else if (seesPlayer)
-            {
-                LostPlayer();
-            }
+        }
+        else if (seesPlayer)
+        {
+            LostPlayer();
         }
     }
 
@@ -57,6 +69,8 @@ public class BossSight : MonoBehaviour
     {
         seesPlayer = true;
         Debug.Log("Boss has seen player.");
+        lightHolder.GetComponent<Light>().intensity = 100;
+        playerManager.BossSawPlayer();
     }
 
     /*
@@ -67,5 +81,18 @@ public class BossSight : MonoBehaviour
         seesPlayer = false;
         Debug.Log("Boss has lost player.");
         timeSeeingPlayer = 0;
+        lightHolder.GetComponent<Light>().intensity = 0;
+        playerManager.BossLostPlayer();
+    }
+
+    /*
+     * Triggers when the boss loses the player after seeing them.
+     */
+    private void BossSeenTooLong()
+    {
+        Debug.Log("Had sight of player for too long");
+        timeSeeingPlayer = -5;
+        playerManager.BossSeenPlayerTooLong(transform.position);
+        lightHolder.GetComponent<Light>().intensity = 1000;
     }
 }
---
+++ Assets/Scripts/PlayerLightManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLightManager : MonoBehaviour
+{
+    [SerializeField] private Light bossSightLightLow = null;
+    [SerializeField] private Light bossSightLightHigh = null;
+
+    [SerializeField] private Light flashlightDim = null;
+    [SerializeField] private Light flashlightBright = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bossSightLightLow.enabled = false;
+        bossSightLightHigh.enabled = false;
+
+        flashlightDim.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (!flashlightDim.enabled && !flashlightBright.enabled)
+            {
+                flashlightDim.enabled = true;
+            }
+            else if (flashlightDim.enabled && !flashlightBright.enabled)
+            {
+                flashlightBright.enabled = true;
+                flashlightDim.enabled = false;
+            }
+            else
+            {
+                flashlightBright.enabled = false;
+                flashlightDim.enabled = false;
+            }
+        }
+    }
+
+    public void BossSawPlayer()
+    {
+        bossSightLightLow.enabled = true;
+        bossSightLightHigh.enabled = false;
+    }
+
+    public void BossLostPlayer()
+    {
+        bossSightLightLow.enabled = false;
+        bossSightLightHigh.enabled = false;
+
+    }
+
+    public void BossSeenPlayerTooLong()
+    {
+        StartCoroutine(BossSeenPlayerTooLongEnum());
+    }
+
+    private IEnumerator BossSeenPlayerTooLongEnum()
+    {
+        bossSightLightLow.enabled = false;
+        bossSightLightHigh.enabled = true;
+        yield return new WaitForSeconds(1.5f);
+        bossSightLightLow.enabled = false;
+        bossSightLightHigh.enabled = false;
+    }
+}
